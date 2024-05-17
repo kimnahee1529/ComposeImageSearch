bcb8ea8231a0310bab17ff06b2187562bd9290b1
nhkim <nhkim1529@gmail.com>
2024-05-17 11:00:19 +0900

    [Design] Color 추가

---
+++ app/src/main/java/com/nhkim/composeimagesearch/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.nhkim.composeimagesearch.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val PrimaryMainColor = Color(0xFFFC96B6)
+val PrimaryColor = Color(0xFFFFC5D7)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
